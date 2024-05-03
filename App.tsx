f88243b6199a35c8e144261f37e03540d8935a88
Kealym Bromley <kealymmb@gmail.com>
2024-05-03 15:49:51 +0200

    Utilized provider

--- App.tsx
+++ App.tsx
@@ -6,51 +6,38 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  Button,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 // Below is library adding screens of app
-import AddBook from '../bookWorm/android/screens/addBook';
-import HomeScreen from'./android/screens/HomeScreen';
-import History from './android/screens/history';
-import Genre from './android/screens/genre';
+
+// Providers
+import {BookProvider} from './src/providers/bookProvider';
+import HomeScreen from './src/screens/HomeScreen';
+import AddBook from './src/screens/addBook';
+import History from './src/screens/history';
+import Genre from './src/screens/genre';
 
 const Stack = createNativeStackNavigator();
 // Stack Navigator allowing navigation between screens
-function App(): React.JSX.Element{
-  return(
-<NavigationContainer>
-  <Stack.Navigator initialRouteName='BookWorm'>
-    <Stack.Screen name='BookWorm' component={HomeScreen}/>
-    <Stack.Screen name='Add Book' component={AddBook}/>
-    <Stack.Screen name='History' component={History}/>
-    <Stack.Screen name='Genre' component={Genre}/>
-  </Stack.Navigator>
-</NavigationContainer>
+function App(): React.JSX.Element {
+  return (
+    <BookProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="BookWorm">
+          <Stack.Screen name="BookWorm" component={HomeScreen} />
+          <Stack.Screen name="Add Book" component={AddBook} />
+          <Stack.Screen name="History" component={History} />
+          <Stack.Screen name="Genre" component={Genre} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </BookProvider>
   );
 }
+
 // Styles to be shared
-const styles = StyleSheet.create({
-  
+export const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 24,
     color: '#FFFFFF',
@@ -59,16 +46,16 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   sectionHeader: {
-    backgroundColor:'#FFF7E1',
+    backgroundColor: '#FFF7E1',
     textAlign: 'center',
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
   },
-  sectionText:{
+  sectionText: {
     fontStyle: 'italic',
     textAlign: 'center',
-    backgroundColor:'#FFF7E1',
+    backgroundColor: '#FFF7E1',
   },
   buttonHeader: {
     fontWeight: '700',
